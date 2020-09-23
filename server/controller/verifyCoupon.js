fbe6add2538c79b8bdef0c4f128f011619533153
samridh <samridhharsh@gmail.com>
2020-09-24 02:13:30 +0530

    threshhold amount logic added

--- server/controller/verifyCoupon.js
+++ server/controller/verifyCoupon.js
@@ -4,20 +4,25 @@ const verifyCoupon = async (body) => {
     const Coupon = await coupon.findOne({ value: body?.coupon })
     if (Coupon) {
         if (new Date().getTime() > Coupon.startDate.getTime() && new Date().getTime() < Coupon.endDate.getTime()) {
-            let discount;
-            if (Coupon.type === 'percentage') {
-                discount = (body.total * Coupon.value)/100
+            if (body.total >= Coupon.thresholdAmount) {
+                let discount;
+                if (Coupon.type === 'percentage') {
+                    discount = (body.total * Coupon.value)/100
+                } else {
+                    discount = Coupon.value
+                }
+
+                return {
+                    inRange: true,
+                    discount,
+                    finalAmount: body.total - discount
+                }
             } else {
-                discount = Coupon.value
+                return {
+                    inRange: false,
+                    message: `Need to add more items worth ${Coupon.thresholdAmount - body.total} to the list for this coupon to be enabled.`
+                }
             }
-
-            const data  = {
-                inRange: true,
-                discount,
-                finalAmount: body.total - discount
-            }
-            return data
-
         } else {
             return {
                 inRange: false,
